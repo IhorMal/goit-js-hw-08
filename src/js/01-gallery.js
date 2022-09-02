32998affccc1eb81016d83654c7e25c57b6ea56f
Igor <ihormalyuha@gmail.com>
2022-09-02 21:37:21 +0300

    Виконане ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function creationGalleryElements({ preview, original, description }) {
+   
+    return `<li>
+        <a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+          />
+        </a>
+      </li>`; 
+}
+
+const createdGallery = galleryItems.map(creationGalleryElements).join('');
+gallery.innerHTML = createdGallery;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+    captionDelay: 250,
+    navText: ['&#10913', '&#10914'], 
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector('.feedback-form')
+const data = {}
+
+populateTextarea()
+
+form.addEventListener('submit', resetForm);
+
+function resetForm(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state")
+}
+
+form.addEventListener('input', throttle(addLocalStorage, 300));
+
+function addLocalStorage(e) {
+    data[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state", JSON.stringify(data));
+}
+
+function populateTextarea() {
+    const storagePars = JSON.parse(localStorage.getItem("feedback-form-state"));
+   if (storagePars === null) {
+    return;
+   }
+    
+    form.elements.email.value = storagePars.email || '';
+    form.elements.message.value = storagePars.message || '';
+}
